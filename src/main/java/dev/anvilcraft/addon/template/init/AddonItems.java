91dafea7e05d938c4e985e7558c67507c2900718
人哉肥 <chuen6@qq.com>
2026-07-08 16:52:55 +0800

    Initial commit

---
+++ src/main/java/dev/anvilcraft/addon/template/init/AddonItems.java
@@ -0,0 +1,19 @@
+package dev.anvilcraft.addon.template.init;
+
+import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.world.item.Item;
+
+import static dev.anvilcraft.addon.template.AnvilCraftAddonTemplate.REGISTRATE;
+
+public class AddonItems {
+    static {
+        REGISTRATE.defaultCreativeTab(AddonItemGroups.ADDON_ITEMS.getKey());
+    }
+
+    public static final ItemEntry<Item> EXAMPLE_ITEM = REGISTRATE
+        .item("example_item", Item::new)
+        .register();
+
+    public static void register() {
+    }
+}
